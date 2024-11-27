14b274616c4e321ac204138c25d698ce52388aae
Keepgoingcr007 <keepgoingcr7@gmail.com>
2024-11-27 15:02:27 -0600

    new-api file updated //& is ready to use

--- api-new/variable.tf
+++ api-new/variable.tf
@@ -1,3 +1,29 @@
-variable "region" {
-  default = "us-east-1"
+variable "api_resources" {
+  type = list(object({
+    resource_name = string
+    methods  = list(string)
+  }))
+  default = [
+    {
+      resource_name = "agent"
+      methods  = ["GET", "POST"]
+    },
+    {
+      resource_name = "employer"
+      methods  = ["PUT", "DELETE"]
+    }
+  ]
 }
+# #
+# variable "api_resources" {
+#   default = [
+#     {
+#       resource_name = "agent"
+#       methods       = ["GET", "POST"]
+#     },
+#     {
+#       resource_name = "employer"
+#       methods       = ["PUT", "DELETE"]
+#     }
+#   ]
+# }
